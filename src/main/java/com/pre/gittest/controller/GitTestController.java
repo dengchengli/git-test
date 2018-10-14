6f7b1d1fa58058c5beec74544e5d396039b7085b
dengchengli <dengchengli@iqianjin.com>
2018-10-14 17:08:19 +0800

    dev-first change !

--- src/main/java/com/pre/gittest/controller/GitTestController.java
+++ src/main/java/com/pre/gittest/controller/GitTestController.java
@@ -2,4 +2,11 @@ package com.pre.gittest.controller;
 
 
 public class GitTestController {
+    public String first() {
+        System.out.println("dev-first-change");
+    }
+
+    public String firstChange(){
+        System.out.println("dev-first-change !");
+    }
 }
