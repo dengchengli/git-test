f4ea4b77866c917b0d97a07852c6ef9266eb8675
dengchengli <dengchengli@iqianjin.com>
2018-10-14 17:33:19 +0800

    Revert "dev-first change !"
    This reverts commit 6f7b1d1fa58058c5beec74544e5d396039b7085b.

--- src/main/java/com/pre/gittest/controller/GitTestController.java
+++ src/main/java/com/pre/gittest/controller/GitTestController.java
@@ -2,11 +2,4 @@ package com.pre.gittest.controller;
 
 
 public class GitTestController {
-    public String first() {
-        System.out.println("dev-first-change");
-    }
-
-    public String firstChange(){
-        System.out.println("dev-first-change !");
-    }
 }
